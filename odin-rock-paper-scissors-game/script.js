d090d1c4fb9519157215a3d8bd43efefe1ad8a53
Jacob Vernau <jacobvernau@gmail.com>
2021-11-15 10:47:07 -0700

    functions complete
    functions for AI choice and user choice completed.

--- odin-rock-paper-scissors-game/script.js
+++ odin-rock-paper-scissors-game/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// AI choice
+const computerPlay = function () {
+  let x = Math.floor(Math.random() * 3);
+  // returns a number between 0-2
+  console.log(x);
+  if (x === 0) {
+    return 'rock';
+  } else if (x === 1) {
+    return 'paper';
+  } else if (x === 2) {
+    return 'scissors';
+  }
+};
+
+// player's choice
+const playerSelection = function () {
+  const userAnswer = prompt('Rock, paper, or scissors?').toLowerCase();
+  // always set user input to lowercase for comparison ease
+  return userAnswer;
+};
+
+console.log(computerPlay(), playerSelection());
