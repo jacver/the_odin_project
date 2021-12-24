e3e0e9d1c47253c6b356ec8250ddaed1b3cd2b56
Jacob Vernau <jacobvernau@gmail.com>
2021-12-24 14:03:12 -0700

    buttons
    buttons and selectors implemented. Score not working here.

--- odin-rock-paper-scissors-game/script.js
+++ odin-rock-paper-scissors-game/script.js
@@ -6,148 +6,113 @@ const btnRock = document.querySelector('.player-choice-rock');
 const btnPaper = document.querySelector('.player-choice-paper');
 const btnScissors = document.querySelector('.player-choice-scissors');
 
-// functions
+let labelSelections = document.getElementById('selections');
+let labelScoreboard = document.getElementById('scoreboard');
 
-// buttons
+// pre-game
+let playerSelection = '';
+
+let playerScore = 0;
+let computerScore = 0;
+
+labelSelections.textContent = 'Rock, paper, or scissors?';
+labelScoreboard.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+
+// functions
 
 // AI selection
 const computerPlay = function () {
   let x = Math.floor(Math.random() * 3);
   // returns a number between 0-2
   if (x === 0) {
-    console.log('AI: rock');
-    return 'rock';
+    return '✊';
   } else if (x === 1) {
-    console.log('AI: paper');
-    return 'paper';
+    return '🤚';
   } else if (x === 2) {
-    console.log('AI: scissors');
-    return 'scissors';
+    return '✌';
   }
 };
 
-// Player selection via prompt
-// let playerPlay = function () {
-//   let userAnswer = prompt('Rock, paper, or scissors?').trim().toLowerCase();
-//   console.log(`User: ${userAnswer}`);
-//   return userAnswer;
-// };
-
-// one round of play
-const playRound = function (playerSelection, computerSelection) {
-  if (playerSelection === 'rock') {
-    if (computerSelection === 'scissors') {
-      console.log('win round');
-      return 'win';
-    } else if (computerSelection === 'paper') {
-      console.log('lose round');
-      return 'lose';
-    } else {
-      console.log('tie round');
-      return 'tie';
-    }
-  }
-  if (playerSelection === 'paper') {
-    if (computerSelection === 'rock') {
-      console.log('win round');
-      return 'win';
-    } else if (computerSelection === 'scissors') {
-      console.log('lose round');
-      return 'lose';
-    } else {
-      console.log('tie round');
-      return 'tie';
-    }
-  }
-  if (playerSelection === 'scissors') {
-    if (computerSelection === 'paper') {
-      console.log('win round');
-      return 'win';
-    } else if (computerSelection === 'rock') {
-      console.log('lose round');
-      return 'lose';
-    } else {
-      console.log('tie round');
-      return 'tie';
-    }
-  }
-};
-
-const game = function () {
-  let rounds = 1;
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 1; i <= rounds; i++) {
-    // player selection
-    let playerSelection = playerPlay();
-    // computer selection
-    let computerSelection = computerPlay();
-
-    // one round
-    let round = playRound(playerSelection, computerSelection);
-
-    // scoring and post-round messaging
-    if (round === 'win') {
-      playerScore++;
-      console.log(
-        `Round ${i}: You win. You: ${playerScore} points, Computer: ${computerScore} points`
-      );
-    }
-
-    if (round === 'tie') {
-      console.log(
-        `Round ${i}: Tie. You: ${playerScore} points, Computer: ${computerScore} points`
-      );
-    }
-    if (round === 'lose') {
-      computerScore++;
-      console.log(
-        `Round ${i}: You lose. You: ${playerScore} points, Computer: ${computerScore} points`
-      );
-    }
-  }
-
-  // after all rounds - final score
-  if (playerScore > computerScore) {
-    console.log(`You win! ${playerScore} to ${computerScore}`);
-  }
-  if (playerScore < computerScore) {
-    console.log(`You lose! ${playerScore} to ${computerScore}`);
-  }
-  if (computerScore === playerScore) {
-    console.log(`Game ends in a tie! ${playerScore} to ${computerScore}`);
-  }
-};
-
-// game();
+// player selection buttons
 
 btnRock.addEventListener('click', function () {
   // player pick
   let playerSelection = 'rock';
-  console.log('user: rock');
+  console.log(playerSelection);
   // ai pick
   let computerSelection = computerPlay();
+  console.log(computerSelection);
   // play round
-  playRound(playerSelection, computerSelection);
+  labelSelections.textContent = `✊ to ${computerSelection}`;
 });
 
 btnPaper.addEventListener('click', function () {
   //player pick
   let playerSelection = 'paper';
-  console.log('user: paper');
   // ai pick
   let computerSelection = computerPlay();
   // play round
+  labelSelections.textContent = `🤚 to ${computerSelection}`;
   playRound(playerSelection, computerSelection);
 });
 
 btnScissors.addEventListener('click', function () {
   // player pick
   let playerSelection = 'scissors';
-  console.log('user: scissors');
   // ai pick
   let computerSelection = computerPlay();
   // play round
+  labelSelections.textContent = `✌ to ${computerSelection}`;
   playRound(playerSelection, computerSelection);
 });
+
+// one round of play
+
+const playRound = function (playerSelection, computerSelection) {
+  if (playerSelection === 'rock') {
+    if (computerSelection === '✌') {
+      playerScore++;
+      return 'win';
+    } else if (computerSelection === '🤚') {
+      return 'lose';
+    } else {
+      return 'tie';
+    }
+  }
+  if (playerSelection === 'paper') {
+    if (computerSelection === '✊') {
+      return 'win';
+    } else if (computerSelection === '✌') {
+      return 'lose';
+    } else {
+      return 'tie';
+    }
+  }
+  if (playerSelection === 'scissors') {
+    if (computerSelection === '🤚') {
+      return 'win';
+    } else if (computerSelection === '✊') {
+      return 'lose';
+    } else {
+      return 'tie';
+    }
+  }
+
+  // scoring
+  if (result === 'win') {
+  }
+  if (result === 'lose') {
+    computerScore++;
+  }
+};
+
+// after all rounds - final score
+if (playerScore > computerScore) {
+  console.log(`You win! ${playerScore} to ${computerScore}`);
+}
+if (playerScore < computerScore) {
+  console.log(`You lose! ${playerScore} to ${computerScore}`);
+}
+if (computerScore > 0 && computerScore === playerScore) {
+  console.log(`Game ends in a tie! ${playerScore} to ${computerScore}`);
+}
