df41ab19f199bc93bdbf8d0ec7c81ee14653ed56
Jacob Vernau <jacobvernau@gmail.com>
2022-01-30 21:15:18 -0700

    remaking grid

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Implementation needs
 // button to create the grid
@@ -7,29 +7,29 @@
 
 // Selectors
 
-const btnNewGrid = document.querySelector(".btn-new-grid");
-const gridContainer = document.querySelector(".grid-container");
+const btnNewGrid = document.querySelector('.btn-new-grid');
+const gridContainer = document.querySelector('.grid-container');
 
 // Creating Grid
 
 const createGrid = function (rows, columns) {
-  gridContainer.style.setProperty("--grid-rows", rows);
-  gridContainer.style.setProperty("--grid-columns", columns);
+  gridContainer.style.setProperty('--grid-rows', rows);
+  gridContainer.style.setProperty('--grid-columns', columns);
 
   for (let i = 0; i < rows * columns; i++) {
-    let cell = document.createElement("div");
-    gridContainer.appendChild(cell).className = "grid-item";
+    let cell = document.createElement('div');
+    gridContainer.appendChild(cell).className = 'grid-item';
   }
 };
 createGrid(16, 16);
 
 // Trailing effect
 
-const cells = document.querySelectorAll(".grid-item");
+const cells = document.querySelectorAll('.grid-item');
 
 cells.forEach((cell) => {
-  cell.addEventListener("mouseenter", () => {
-    cell.classList.add("hover");
+  cell.addEventListener('mouseenter', () => {
+    cell.classList.add('hover');
   });
 });
 
@@ -37,16 +37,27 @@ cells.forEach((cell) => {
 
 // /// /// WORKING ON: deleting entire grid to make a new one with measurements from prompt
 
-const clearGrid = btnNewGrid.addEventListener("click", function () {
+const clearGrid = btnNewGrid.addEventListener('click', function () {
   // clear cells
   cells.forEach((cell) => {
-    cell.classList.remove("hover");
+    cell.classList.remove('hover');
   });
 
-  // prompt user for new grid size
-  let rows = prompt("How many squares would you like on each side?");
+  // Delete existing grid
+  cells.forEach((cell) => {
+    cell.remove();
+  });
+
+  // prompt user for new grid size and collect input
+  const howManyRows = prompt(
+    'How many squares per side should your new grid be?'
+  );
+  let rows = Number(howManyRows);
   let columns = rows;
 
   // create new grid with user specs
+  gridContainer.style.gridTemplateRows = `repeat(${rows}, 25px)`;
+  gridContainer.style.gridTemplateColumns = `repeat(${columns}, 25px)`;
+
   createGrid(rows, columns);
 });
